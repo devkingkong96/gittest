9f872ef28c65e78120fac429579e91efa062c586
GOODEE <GOODEE@DESKTOP-IO5RID4>
2023-10-17 16:57:06 +0900

    bs_메소드 만들고 이것저것

---
+++ gittest/src/com/gittest/controller/GitController.java
@@ -0,0 +1,10 @@
+package com.gittest.controller;
+
+public class GitController {
+	public void test() {
+		String name="bs";
+	}
+	public String msg() {
+		return "git재미있다";
+	}
+}
---
+++ gittest/src/com/gittest/run/Main.java
@@ -0,0 +1,9 @@
+package com.gittest.run;
+
+public class Main {
+
+	public static void main(String[] args) {
+		System.out.println("안녕 git 반가워!");
+	}
+
+}
