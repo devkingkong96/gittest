ded90b6a7f04adf3558014554753d4af303ce743
GOODEE <GOODEE@DESKTOP-IO5RID4>
2023-10-18 09:12:42 +0900

    bs_main 메소드 수정

--- gittest/src/com/gittest/run/Main.java
+++ gittest/src/com/gittest/run/Main.java
@@ -1,9 +1,11 @@
 package com.gittest.run;
 
+import com.gittest.controller.GitController;
+
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("안녕 git 반가워!");
-		System.out.println("난 EDV브랜치 우하하");
+		new GitController().test();
 	}
+
 }
